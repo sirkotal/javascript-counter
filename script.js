c735462620b5571cba1fbb09ea11507d927eda14
sirkotal <lordkotal2002@gmail.com>
2022-12-17 12:37:46 +0000

    Added a script file

---
+++ script.js
@@ -0,0 +1,13 @@
+let my_alpha
+let my_string = "Hello World!"
+let my_number = 42
+let my_array = [11, "Hello", "42", "World"]
+let my_boolean = true;
+
+let my_class = {
+    name: "Mr. Gus",
+    goal: "Learn JavaScript",
+    age: 20,
+}
+
+let my_button = document.querySelector("#id") // reference to HTML variable
